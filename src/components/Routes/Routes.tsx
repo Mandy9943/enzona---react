c2d6e6424437be753b540df6a1e503a4f5b73877
Armando <armandoc9943@gmail.com>
2021-02-04 05:20:53 -0500

    rutas con efecto

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,20 +1,36 @@
 import React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import {
+  Redirect,
+  Route,
+  Switch,
+  RouteComponentProps,
+  withRouter,
+} from "react-router-dom";
 import Config from "../Config/Config";
 import Friends from "../Friends/Friends";
 import Home from "../Home/Home";
 import Notification from "../Notification/Notification";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "./Rotes.css";
 
-const Routes: React.FC = () => {
+const Routes: React.FC<RouteComponentProps> = (props) => {
   return (
-    <Switch>
-      <Redirect exact={true} from="/" to="/home" />
-      <Route path="/home" component={Home} />
-      <Route path="/notifications" component={Notification} />
-      <Route path="/friends" component={Friends} />
-      <Route path="/config" component={Config} />
-    </Switch>
+    <TransitionGroup>
+      <CSSTransition
+        key={props.location.key}
+        timeout={500}
+        classNames="animate"
+      >
+        <Switch>
+          <Redirect exact={true} from="/" to="/home" />
+          <Route path="/home" component={Home} />
+          <Route path="/notifications" component={Notification} />
+          <Route path="/friends" component={Friends} />
+          <Route path="/config" component={Config} />
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
-export default Routes;
+export default withRouter(Routes);
