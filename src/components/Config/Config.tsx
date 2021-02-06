e410e6a876de7e9c37c86eb2d2c999ac5d99f071
Armando <armandoc9943@gmail.com>
2021-02-06 02:35:52 -0500

    quitando Link para build en netlify

--- src/components/Config/Config.tsx
+++ src/components/Config/Config.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
 import { OneOfListOPtions, OptionList } from "../Options/Options";
 import PanelHeader from "../PanelHeader/PanelHeader";
 import "./Config.css";
