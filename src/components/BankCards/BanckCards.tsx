807130a7a6907f69cf259fe123cda0118e8848c6
Armando <armandoc9943@gmail.com>
2021-02-10 01:39:22 -0500

    quitando codigo que no se usa para q compile en netlify

--- src/components/BankCards/BanckCards.tsx
+++ src/components/BankCards/BanckCards.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import HeaderBar from "../HeaderBar/HeaderBar";
 import "./BankCards.css";
 const BanckCards: React.FC = (props) => {
   return <div className="bank-cards">{props.children}</div>;
